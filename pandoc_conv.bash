dd165a6651ea3a337f0c340cf01bb8d15ac29079
unforswearing <al.charity@gmail.com>
2024-09-21 16:34:56 -0400

    add reference to pandocs '--default' arg

--- pandoc_conv.bash
+++ pandoc_conv.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# using a yaml file to store default pandoc arguments
+# see: https://pandoc.org/MANUAL.html#defaults-files
+
 # pandoc --defaults=settings.yaml -i input.md -o output.file
 # pandoc --defaults=collection.yaml
